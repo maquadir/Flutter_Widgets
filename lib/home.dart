890963f24881a7a28e7e521177a60db9bf893598
adeemad <adeem@adabdigital.com>
2023-03-24 22:08:07 +1100

    Initial commit

---
+++ lib/home.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import 'fooderlich_theme.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+
+  int _selectedIndex = 0;
+
+  static List<Widget> pages = <Widget> [
+    Container(color: Colors.red),
+    Container(color: Colors.green),
+    Container(color: Colors.amber),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      // TODO: Style the title
+      appBar: AppBar(title: const Text('Fooderlich')),
+      // TODO: Style the body text
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        selectedItemColor: Theme
+            .of(context)
+            .textSelectionTheme
+            .selectionColor,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.card_giftcard), label: 'Card1'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.card_giftcard), label: 'Card2'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.card_giftcard), label: 'Card3')
+        ],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+}
