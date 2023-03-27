ca8478d8c79b4e15dd450c5158094f85392a9363
adeemad <adeem@adabdigital.com>
2023-03-28 06:57:11 +1100

    start of card3.dart

--- lib/card3.dart
+++ lib/card3.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'fooderlich_theme.dart';
 
 class Card3 extends StatelessWidget {
   const Card3({super.key});
@@ -14,7 +17,33 @@ class Card3 extends StatelessWidget {
                     image: AssetImage('assets/mag2.png'), fit: BoxFit.cover),
                 borderRadius: BorderRadius.all(Radius.circular(10.0))),
             child: Stack(
-              children: [],
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.6),
+                    borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Icon(
+                        Icons.book,
+                        color: Colors.white,
+                        size: 40,
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        'Recipe Trends',
+                        style: FooderlichTheme.darkTextTheme.headline2,
+                      ),
+                      const SizedBox(height: 30),
+                    ],
+                  ),
+                ),
+              ],
             )));
   }
 }
