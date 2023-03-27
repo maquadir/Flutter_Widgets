8c7f74cb498135186dbe0070a991fb639882be50
adeemad <adeem@adabdigital.com>
2023-03-27 21:07:15 +1100

    add text and rotated box text inside stack

--- lib/card2.dart
+++ lib/card2.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
 import 'author_card.dart';
+import 'fooderlich_theme.dart';
 
 class Card2 extends StatelessWidget {
   const Card2({super.key});
@@ -23,6 +24,29 @@ class Card2 extends StatelessWidget {
             title: 'Developer',
             imageProvider: AssetImage('assets/author_katz.jpeg'),
           ),
+          Expanded(
+              child: Stack(
+            children: [
+              Positioned(
+                bottom: 16,
+                right: 16,
+                child: Text(
+                  'Recipe',
+                  style: FooderlichTheme.lightTextTheme.headline1,
+                ),
+              ),
+              Positioned(
+                  bottom: 70,
+                  left: 16,
+                  child: RotatedBox(
+                    quarterTurns: 3,
+                    child: Text(
+                      'Smoothies',
+                      style: FooderlichTheme.lightTextTheme.headline1,
+                    ),
+                  )),
+            ],
+          ))
         ],
       ),
     ));
